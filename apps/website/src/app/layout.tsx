4ea06108a0c2ab351db331b9d6a5f68d49e5b617
Daniel Gonzalez <daniel.gonzalez10152@gmail.com>
2026-03-30 15:13:30 -0700

    Spanish

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="esp">
 			<AnalyticsInitializer />
 			<body className={"antialiased"}>{children}</body>
 		</html>
